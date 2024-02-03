c5c8383dc1fedc55c6de04ea9d96bc18bc2a369a
Jonathyn Stiverson <43549238+ProgrammingLogic@users.noreply.github.com>
2024-02-03 16:14:41 -0600

    Implement mob_timer, pass new_game() into _ready()

--- Main.gd
+++ Main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,8 +26,27 @@ func new_game():
 
 
 func _on_mob_timer_timeout():
-	pass # Replace with function body.
-
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	# Set the mob's direction perpendicular to the path direction
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	mob.position = mob_spawn_location.position
+	
+	# Add randomness to direction
+	direction += randf_range (-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# Choose the velocity for the mob 
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# Spawn the mob
+	add_child(mob)
+	
 
 func _on_score_timer_timeout():
 	score += 1
