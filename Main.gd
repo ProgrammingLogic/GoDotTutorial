7ddbfc3c47d2604525eda60537b65b033d37e1c3
Jonathyn Stiverson <43549238+ProgrammingLogic@users.noreply.github.com>
2024-02-03 16:37:15 -0600

    Update the HUD on new_game()

--- Main.gd
+++ Main.gd
@@ -21,8 +21,13 @@ func game_over():
 
 func new_game():
 	score = 0
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
 
 
 func _on_mob_timer_timeout():
